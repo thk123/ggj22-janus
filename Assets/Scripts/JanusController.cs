cfdaef7c0c639d9d5bd865a05fea14c86ccb76f7
Thomas <thekileyenator@gmail.com>
2022-01-29 13:43:29 +0000

    Add falling logic

--- Assets/Scripts/JanusController.cs
+++ Assets/Scripts/JanusController.cs
@@ -12,6 +12,8 @@ public class JanusController : MonoBehaviour
     private GridEntity gridEntity;
     private GridManager gridManager;
 
+    private bool flightInMotion;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,38 +21,65 @@ public class JanusController : MonoBehaviour
         directionHandler = GetComponent<JanusDirection>();
         gridEntity = GetComponent<GridEntity>();
         gridManager = GameObject.FindObjectOfType<GridManager>();
+        flightInMotion = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        
+        if(!flightInMotion)
         {
-            DoStep(JanusColourMode.White);
-            
+            UpdateFalling();
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                DoStep(JanusColourMode.White);
+                
+            }
+            else if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                DoStep(JanusColourMode.Black);
+            }
         }
-        else if(Input.GetKeyDown(KeyCode.RightArrow))
+    }
+
+    private void UpdateFalling()
+    {
+        var targetCell = gridEntity.CurrentPosition + new Vector2Int(0, -1);
+        if(CanMoveIntoCell(targetCell))
         {
-            DoStep(JanusColourMode.Black);
+            StartCoroutine(FallingRoutine(targetCell));
+            flightInMotion = true;
         }
+    }
+
+    IEnumerator FallingRoutine(Vector2Int fallTo)
+    {
+        gridEntity.CurrentPosition = fallTo;
+        yield return new WaitForSeconds(0.5f);
+        flightInMotion = false;
     }
 
     public void DoStep(JanusColourMode direction)
     {
         var moveDirection = new Vector2Int(DirectionFromMode(direction), 0);
         var targetCell = gridEntity.CurrentPosition + moveDirection;
-        if(gridManager.IsCellInBounds(targetCell))
+        if(CanMoveIntoCell(targetCell))
         {
-            if(CanMoveIntoCell(gridManager.GetCellContents(targetCell)))
-            {
-                directionHandler.CurrentMode = direction;
-                gridEntity.CurrentPosition = targetCell;
-            }
+            directionHandler.CurrentMode = direction;
+            gridEntity.CurrentPosition = targetCell;
         }
     }
 
-    private bool CanMoveIntoCell(GridEntity contents)
+    private bool CanMoveIntoCell(Vector2Int targetCell)
     {
+        if(!gridManager.IsCellInBounds(targetCell))
+        {
+            return false;
+        }
+
+        var contents = gridManager.GetCellContents(targetCell);
+
         if(contents == null)
             return true;
 
