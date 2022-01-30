90625de534e340b3fe0fe273f1e963d1831c0717
Thomas <thekileyenator@gmail.com>
2022-01-30 14:27:48 +0000

    Zoom on the door at the end of a level
    And fix random glitches that started showing up because we are nearing
    the end of the jam

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -9,12 +9,19 @@ public class Door : MonoBehaviour
     bool isLocked;
     private GridManager gridManager;
     private GridEntity gridEntity;
+
+    public Sprite UnlockedDoorImage;
+    private SpriteRenderer Renderer;
+    public float ZoomTime = 0.5f;
+    public float EndOrthoSize = 0.25f;
+
     // Start is called before the first frame update
     void Start()
     {
         isLocked = true;
         gridManager = GameObject.FindObjectOfType<GridManager>();
         gridEntity = GetComponent<GridEntity>();
+        Renderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -30,14 +37,47 @@ public class Door : MonoBehaviour
 
             if(overlappingController != null)
             {
-                gridManager.CompleteLevel();
-                enabled = false;
+                StartCoroutine(ZoomInOnDoor());
+                
+                isLocked = true;
             }
         }
+    }
+
+    IEnumerator ZoomInOnDoor()
+    {
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        foreach(var entityInWorld in GameObject.FindObjectsOfType<GridEntity>())
+        {
+            entityInWorld.enabled = false;   
+        }
+        Vector3 cameraStartPos = Camera.main.transform.position;
+        float cameraStartSize = Camera.main.orthographicSize;
+        var target = GetCentre();
+        target.z = cameraStartPos.z;
+        float t = 0.0f;
+        var curve = AnimationCurve.EaseInOut(0.0f, 0.0f, ZoomTime, 1.0f);
+        while(t < ZoomTime)
+        {
+            var val = curve.Evaluate(t);
+            Camera.main.transform.position = Vector3.Lerp(cameraStartPos, target, val);
+            Camera.main.orthographicSize = Mathf.Lerp(cameraStartSize, EndOrthoSize, val);
+            t += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        gridManager.CompleteLevel();
+
     }
 
     public void Unlock()
     {
         isLocked = false;
+        Renderer.sprite = UnlockedDoorImage;
+    }
+
+    public Vector3 GetCentre()
+    {
+        return Renderer.transform.position;
     }
 }
--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -10,6 +10,12 @@ public class Key : MonoBehaviour
     private GridManager gridManager;
     private Door doorToUnlock;
     private bool pickedUp;
+
+    public float MoveDuration = 0.5f;
+
+    public float ScaleParam = 0.7f;
+    public float ShrinkDuration = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +42,7 @@ public class Key : MonoBehaviour
             //var keyPosition = doorToUnlock.GetComponent<GridEntity>().StartPosition + new Vector2Int(0, 1);
             //gridEntity.StartPosition = keyPosition;
             gridEntity.enabled = false;
+            GetComponent<DirectionVisibility>().enabled = false;
             StartCoroutine(SlideToDoor());
             
             pickedUp = true;
@@ -46,18 +53,28 @@ public class Key : MonoBehaviour
     {
         float t = 0;
         Vector3 startPos = transform.position;
-        var curve = AnimationCurve.EaseInOut(0, 0, 1.0f, 1.0f);
+        var curve = AnimationCurve.EaseInOut(0.0f, 0.0f, MoveDuration, 1.0f);
         while(distanceToDoor() > float.Epsilon * float.Epsilon)
         {
             float pos = curve.Evaluate(t);
-            transform.position = Vector3.Lerp(startPos, doorToUnlock.transform.GetChild(0).position, pos);
+            transform.position = Vector3.Lerp(startPos, doorToUnlock.GetCentre(), pos);
+            t += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        t = 0.0f;
+        var shrinkCurve = AnimationCurve.EaseInOut(0, 0, ShrinkDuration, 1.0f);
+        Vector3 startScale = transform.localScale;
+        while(t < ShrinkDuration)
+        {
+            float pos = shrinkCurve.Evaluate(t);
+            transform.localScale = Vector3.Lerp(startScale, ScaleParam * startScale, pos);
             t += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
         doorToUnlock.Unlock();
     }
 
-    float distanceToDoor() =>(transform.position - doorToUnlock.transform.position).sqrMagnitude;
+    float distanceToDoor() =>(transform.position - doorToUnlock.GetCentre()).sqrMagnitude;
 
     public bool IsVisible()
     {
